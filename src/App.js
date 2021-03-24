ea29fba5ce68d41d1ca5315873d915d1fd1ffdfb
vparkash2014 <57243248+vparkash2014@users.noreply.github.com>
2021-03-24 23:47:39 +1100

    adding banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './containers/Nav';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import Banner from './components/Banner';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+
 
 const App = () => {
   return (
     <>
       <Nav />
+      <Banner title="quick dinner recipes" text="you can make tonight!" icon={faUtensils} />
     </>
   );
 };
