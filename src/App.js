5b7237051eb872e56de4eea9448dc4f923738198
Madd <madd.sheppard@gmail.com>
2021-03-24 23:40:57 +1100

    HeartyFaves container built with Cards and Sticker inside

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './containers/Nav'
+import Nav from './containers/Nav';
+import HeartyFaves from './containers/HeartyFaves/HeartyFaves';
+import recipes from './data/recipes.js';
 
 const App = () => {
   return (
     <>
       <Nav />
+      <HeartyFaves recipes={recipes}/>
     </>
   );
 };
