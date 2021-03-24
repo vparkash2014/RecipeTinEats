946577f1fdb842f9f25c03c067133547f688f8e6
vparkash2014 <57243248+vparkash2014@users.noreply.github.com>
2021-03-24 16:39:20 +1100

    adding SocialIconBtn component

---
+++ src/components/SocialIconBtn/SocialIconBtn.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './SocialIconBtn.module.scss';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+const SocialIconBtn = ({ link='/', icon = faCoffee, size = "lg"}) => {
+    return (
+        <>
+            <a className={styles.Social__btn} href={link}>
+                <FontAwesomeIcon icon={icon} size={size} />
+            </a>
+        </>
+    );
+};
+
+export default SocialIconBtn;
