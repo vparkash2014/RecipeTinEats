a0d461abe815f708bce39fb783a162d8344d107f
Madd <madd.sheppard@gmail.com>
2021-03-24 22:13:47 +1100

    Card component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import SpNavLink from './components/SpNavLink';
 import star from './assets/star.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBarBig from './components/NavBarBig'
+import Card from './components/Card';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import NavBarBig from './components/NavBarBig'
+import recipes from './data/recipes';
+
 
 const App = () => {
   return (
-    <div >
-      <SpNavLink title="Iconic Dishes" icon={star}/>
-    </div>
+    <Card recipeInfo={recipes[0]}/>
   );
 }
 export default App;
