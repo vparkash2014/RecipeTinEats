78885b4e64e6c4edd23a5060900783a84196306c
Madd <madd.sheppard@gmail.com>
2021-03-24 17:05:31 +1100

    app.js edited

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import NavLink from './components/NavLink/NavLink';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div >
     </div>
   );
 }
-
 export default App;
